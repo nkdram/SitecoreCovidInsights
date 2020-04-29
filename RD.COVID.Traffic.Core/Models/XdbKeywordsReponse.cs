7fcd5aeab905a103732fd51dff05a117e979855d
nkdram <ramkumar.nambhi@al-enterprise.com>
2020-04-29 14:41:08 +0200

    Created Sitecore Packages

---
+++ RD.COVID.Traffic.Core/Models/XdbKeywordsReponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RD.COVID.Traffic.Core.Models
+{
+    /// <summary>
+    /// Keywords Response
+    /// </summary>
+    public class XdbKeywordsReponse
+    {
+        public IEnumerable<XdbKeywords> Keywords { get; set; }
+        public IEnumerable<XdbKeywords> TopKeywords { get; set; }
+    }
+}
